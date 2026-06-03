eede1093d2adf1f2a59536f984647e60fd47426f
d3nisacookies <131571593+d3nisacookies@users.noreply.github.com>
2026-06-03 16:51:54 +0000

    asdf

--- executor/executor_pipe_utils.c
+++ executor/executor_pipe_utils.c
@@ -25,6 +25,41 @@ static int	count_pipeline_cmds(t_cmd *cmd)
 	return (count);
 }
 
+static void	close_pipeline_fds(int *pipefd, t_pipe_exec *px, int has_next)
+{
+	if (px->fd_in != -1)
+		close(px->fd_in);
+	if (has_next)
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+	}
+}
+
+static void	run_pipeline_execve(t_pipe_exec *px, t_shell *shell)
+{
+	char	*path;
+
+	if (!px->cmd->args || !px->cmd->args[0])
+		exit(0);
+	path = resolve_command_path(shell, px->cmd->args[0]);
+	if (path)
+	{
+		execve(path, px->cmd->args, shell->env);
+		free(path);
+	}
+	executor_exit_exec_error(px->cmd->args[0]);
+}
+
+static void	setup_pipeline_child(t_pipe_exec *px, int *pipefd, int has_next)
+{
+	if (px->fd_in != -1)
+		dup2(px->fd_in, STDIN_FILENO);
+	if (has_next)
+		dup2(pipefd[1], STDOUT_FILENO);
+	close_pipeline_fds(pipefd, px, has_next);
+}
+
 void	wait_pipeline(pid_t *pids, int count, pid_t last_pid, t_shell *shell)
 {
 	int	i;
@@ -47,32 +82,12 @@ void	wait_pipeline(pid_t *pids, int count, pid_t last_pid, t_shell *shell)
 void	execute_pipeline_child(t_pipe_exec *px, t_shell *shell, int *pipefd,
 		int has_next)
 {
-	char	*path;
-
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
-	if (px->fd_in != -1)
-		dup2(px->fd_in, STDIN_FILENO);
-	if (has_next)
-		dup2(pipefd[1], STDOUT_FILENO);
-	if (px->fd_in != -1)
-		close(px->fd_in);
-	if (has_next)
-	{
-		close(pipefd[0]);
-		close(pipefd[1]);
-	}
 	if (apply_redirections(px->cmd) == -1)
 		exit(1);
-	if (!px->cmd->args || !px->cmd->args[0])
-		exit(0);
-	path = resolve_command_path(shell, px->cmd->args[0]);
-	if (path)
-	{
-		execve(path, px->cmd->args, shell->env);
-		free(path);
-	}
-	executor_exit_exec_error(px->cmd->args[0]);
+	setup_pipeline_child(px, pipefd, has_next);
+	run_pipeline_execve(px, shell);
 }
 
 int	init_pipeline_exec(t_pipe_exec *px, t_cmd *cmd, t_shell *shell)
